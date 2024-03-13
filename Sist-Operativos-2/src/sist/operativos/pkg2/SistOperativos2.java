85fbef13bb7a937b9bd2533eb28705c23955997e
Emilgon <97489051+Emilgon@users.noreply.github.com>
2024-03-12 20:42:26 -0400

    commit castro

--- Sist-Operativos-2/src/sist/operativos/pkg2/SistOperativos2.java
+++ Sist-Operativos-2/src/sist/operativos/pkg2/SistOperativos2.java
@@ -2,20 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package Classes;
+package sist.operativos.pkg2;
 
 /**
  *
  * @author Andrés Castro
  */
-public class Main {
+public class SistOperativos2 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        MainUI Main = new MainUI();
-        Main.setVisible(true); 
+        // TODO code application logic here
     }
     
 }
